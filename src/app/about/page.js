ecb9717387adcfd130546cc0fd8ab7789066a1a4
Phytobear <willoh91@gmail.com>
2024-10-02 14:55:38 +0100

    new

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,7 +5,7 @@ export default function AboutPage() {
       <section id="about" className="bg-white p-8 mt-8 rounded-2xl shadow-lg max-w-3xl mx-auto">
         <h2 className="text-3xl font-semibold mb-4">About Me</h2>
         <p className="text-lg text-gray-700">
-          I am a web developer passionate about creating beautiful and functional websites.
+          I am an avid environmentalist, film enthusiast and web developer.
         </p>
       </section>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,10 @@ export default function Home() {
   return (
 
 <body>
-  <div className="relative h-64 bg-gradient-to-r from-slate-500 to-slate-200">
+  <div className="relative h-screen bg-gradient-to-r from-slate-500 to-slate-200">
     <div className="absolute inset-0 flex items-center justify-center text-white">
       <div className= "bg-transparent p-8 rounded-2xl border-2 border-white shadow-lg">
-        <h1 className="text-white text-4xl font-bold relative z-0 flex">William O&aposHalloran</h1>
+        <h1 className="text-white text-4xl font-bold relative z-0 flex">William O&apos;Halloran</h1>
         <nav className="mt-4 text-center">
           <Link href="/about" className="mr-4 hover:underline">About</Link>
           <Link href="/projects" className="mr-4 hover:underline">Project</Link>
